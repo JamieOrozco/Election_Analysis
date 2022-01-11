ca80d8d7be6a25fd40d843091db92547cb168898
JamieOrozco <95591222+JamieOrozco@users.noreply.github.com>
2022-01-11 13:23:15 -0600

    Add files via upload

---
+++ PyPoll.py
@@ -0,0 +1,19 @@
+# Open the data file.
+# Write down the names of all the candidates.
+# Add a vote count for each candidate.
+# Get the total votes for each candidate.
+# Get the total votes cast for the election.
+
+# Import the datetime class from the datetime module.
+import datetime as dt
+# Use the now() attribute on the datetime class to get the present time.
+now = dt.datetime.now()
+# Print the present time.
+print("The time right now is ", now)
+
+import csv
+dir(csv)
+
+
+
+
